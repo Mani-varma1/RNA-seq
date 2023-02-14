e288084b6a364ad1ae56109bab470acf620e7618
Mani <93208890+Mani-varma1@users.noreply.github.com>
2023-02-14 23:27:11 +0000

    Update alignment.sh

--- RNA_seq/mus_rna/scripts/alignment.sh
+++ RNA_seq/mus_rna/scripts/alignment.sh
@@ -6,7 +6,9 @@ GENBUILD=~/Desktop/nf_tut/rna_seq/mus_rna/output/gencode_build
 gunzip -c ${GENBUILD}/GRCm39.primary_assembly.genome.fa.gz > ${GENBUILD}/GRCm39.primary_assembly.genome.fa
 gunzip -c ${GENBUILD}/gencode.vM32.primary_assembly.annotation.gtf.gz > ${GENBUILD}/gencode.vM32.primary_assembly.annotation.gtf
 
-STAR --runThreadN 6 --runMode genomeGenerate \
+
+## --genomeSAsparseD, --limitGenomeGenerateRAM, --genomeSAindexNb flags for reducing RAM requirements but runs slower
+STAR --runThreadN 4 --runMode genomeGenerate --genomeSAsparseD 3 --limitGenomeGenerateRAM 16000000000 --genomeSAindexNbases 12 \
 	--genomeDir ${GENBUILD}/starIndex --genomeFastaFiles ${GENBUILD}/GRCm39.primary_assembly.genome.fa \
 	--sjdbGTFfile ${GENBUILD}/gencode.vM32.primary_assembly.annotation.gtf
 
